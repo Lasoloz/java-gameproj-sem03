551c580f2ef36a1b7171c71419765d80dc1dc201
László Heim <heim97laszlo@gmail.com>
2017-11-30 10:05:54 +0200

    [proto] Generated libgdx project skeleton

---
+++ desktop/src/com/github/lasoloz/gameproj/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.lasoloz.gameproj.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.lasoloz.gameproj.GameProj;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameProj(), config);
+	}
+}
